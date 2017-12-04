a85339a1688ff3809598cbaba3193fde9c217821
Maarten Bicknese <maarten.bicknese@gmail.com>
2017-12-04 20:43:41 +0100

    Implement relative date component
    I've been fooling around for about an hour with this and did not commit
    in frustration of not getting it to do what I wanted it to do. In the
    end decided to drop it for now.
    
    Also not going to work in 'sessions' anymore. As I've noticed I would
    not start on this when I don't feel like I've enough time on my hands to
    do an actual session.

--- src/Components/Atoms/RelativeDate/RelativeDate.jsx
+++ src/Components/Atoms/RelativeDate/RelativeDate.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Moment from 'moment'
 
 const propTypes = {
   date: PropTypes.string.isRequired,
   relativeTo: PropTypes.string
 }
-const RelativeDate = ({ date, relativeTo = '' }) => (
-  <div>{date}</div>
-)
+class RelativeDate extends Component {
+  renderDate (dateString) {
+    const date = new Moment(dateString)
+    if (date.isSame(new Date(), 'week')) {
+      return Moment(date).startOf('minute').fromNow()
+    }
+    return dateString // .format('LLL')
+  }
+
+  render () {
+    const { date, relativeTo = '' } = this.props
+    return (
+      <div>{this.renderDate(date)}</div>
+    )
+  }
+}
 
 RelativeDate.propTypes = propTypes
 export default RelativeDate
--- src/Components/Atoms/RelativeDate/RelativeDate.spec.jsx
+++ src/Components/Atoms/RelativeDate/RelativeDate.spec.jsx
@@ -1,10 +1,16 @@
 /* eslint-env jest */
 import React from 'react'
+import Moment from 'moment'
 import { shallow } from 'enzyme'
 import RelativeDate from './RelativeDate'
 
 it('renders "a minute ago" a minute ago', () => {
-  const date = new Date()
+  const date = new Moment().subtract(0.8, 'minute').format()
   const relativeDate = shallow(<RelativeDate date={date} />)
   expect(relativeDate.text()).toEqual('a minute ago')
+})
+it('renders the date if long ago', () => {
+  const date = new Moment().subtract(10, 'day').format()
+  const relativeDate = shallow(<RelativeDate date={date} />)
+  expect(relativeDate.text()).toEqual(date)
 })
